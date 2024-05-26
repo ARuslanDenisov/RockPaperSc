6afa4c61d2fdce271728ad6f52cd83160d686564
Denisov Ruslan <denisovitapi@gmail.com>
2024-05-26 17:30:07 +0700

    Initial Commit

---
+++ RockPaperSc/RockPaperScApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScApp.swift
+//  RockPaperSc
+//
+//  Created by Руслан on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
